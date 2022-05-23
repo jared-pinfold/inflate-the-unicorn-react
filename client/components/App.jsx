42708533453de450e37a99667f0ecb155526743a
Jared Pinfold <j.pinfold@gmail.com>
2022-05-23 14:34:05 +1200

    copied over some boilerplate resources from original repo

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -16,11 +16,40 @@ const App = () => {
 
   return (
     <>
-    {count}
-    <h1>{greeting}</h1>
-    <button onClick={() => setCount(count + 1)}>Click</button>
+      <h1> JS CARNIVAL: Inflate The Unicorn! </h1>
+		  <div className="container">
+			  <img className="inflate-an-image" src="/images/waiting-for-photo.png" />
+			  <img className="inflate-an-image" src="/images/waiting-for-photo.png" />
+			  <img className="inflate-an-image" src="/images/waiting-for-photo.png" />
+		  </div>
     </>
   )
+
 }
 
 export default App
+
+
+// <!doctype html>
+// <html lang="en">
+// 	<head>
+// 		<meta charset="utf-8">
+// 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0, maximum-scale=1, minimum-scale=1">
+// 		<title>JS CARNIVAL: Inflate The Unicorn!</title>
+// 		<link href="https://fonts.googleapis.com/css?family=Rye&display=swap" rel="stylesheet">
+// 		<link rel="stylesheet" href="../main.css">
+// 	</head>
+// 	<body>
+
+// 		<h1> JS CARNIVAL: Inflate The Unicorn! </h1>
+
+// 		<div class="container">
+// 			<img class="inflate-an-image" src="./images/waiting-for-photo.png" >
+// 			<img class="inflate-an-image" src="./images/waiting-for-photo.png" >
+// 			<img class="inflate-an-image" src="./images/waiting-for-photo.png" >
+// 		</div>
+
+// 		<script src="inflate-the-unicorn.js"></script>
+// 	</body>
+
+// </html>
